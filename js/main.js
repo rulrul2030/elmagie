f6260439df76892219911cfde0c2d0ad65e4b3ba
rulrul2030 <maerdi27@naver.com>
2025-01-05 17:44:23 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   $(".gnb .depth2").hide();
 
-  $(".gnb > li").hover(function(){
+  $(".gnb > li").hover(function () {
     $(this).find(".depth2").stop().slideToggle();
   });
 
   var reviewList = new Swiper(".review-list", {
     slidesPerView: 5,
     spaceBetween: 30,
-    loop : true,
+    loop: true,
 
     autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
+      delay: 2500,
+      disableOnInteraction: false,
     },
 
   });
-    
+
+  // AOS 초기화
+  AOS.init({
+    once: true, // 스크롤 다시 올려도 애니메이션 실행
+    offset: 120, // 애니메이션 시작 위치
+  });
+
 });
