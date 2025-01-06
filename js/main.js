ba939fbde607bb330079fcf1dddee21f2f310943
rulrul2030 <maerdi27@naver.com>
2025-01-06 23:28:08 +0900

    .

--- js/main.js
+++ js/main.js
@@ -18,10 +18,9 @@ $(document).ready(function () {
 
   });
   
-  // AOS 초기화
   AOS.init({
-    once: true, // 스크롤 다시 올려도 애니메이션 실행
-    offset: 120, // 애니메이션 시작 위치
+    once: true, 
+    offset: 120, 
   });
 
 });
